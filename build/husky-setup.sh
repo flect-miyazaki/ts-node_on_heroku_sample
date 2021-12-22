881842a01f13a9bc6b2327015c9b8a8ffd5fd1d1
shummamiyazaki <shumma.miyazaki@flect.co.jp>
2021-12-22 22:07:26 +0900

    検証中に邪魔なのでhusky無効化

--- build/husky-setup.sh
+++ build/husky-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 if [ -d ./.git ]; then
-    npx husky install && npx husky set .husky/pre-push "yarn lint:fix"
+    # npx husky install && npx husky set .husky/pre-push "yarn lint:fix"
 else
     echo "skip husky init setting"
     exit 0
